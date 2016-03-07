975f9d5ba072bdb5c798e021367836873b15e2b8
feng_ye <feng_ye@fengyemoMacBook.lan>
2016-03-08 00:07:33 +0800

    Fixing drifting

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ HOST, PORT = 'dex', 9999
 def main():
     print("Start")
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK)
-    window = SDL_CreateWindow(b"Hello World",
+    window = SDL_CreateWindow(b"PyBrickPiTank Client",
                               SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                               592, 460, SDL_WINDOW_SHOWN)
     windowsurface = SDL_GetWindowSurface(window)
@@ -24,15 +24,10 @@ def main():
     l_axis = 0 + 128
     r_axis = 0 + 128
 
-    # image = SDL_LoadBMP(b"exampleimage.bmp")
-    # SDL_BlitSurface(image, None, windowsurface, None)
-
-    # SDL_UpdateWindowSurface(window)
-    # SDL_FreeSurface(image)
-
     running = True
     event = SDL_Event()
     while running:
+
         while SDL_PollEvent(ctypes.byref(event)) != 0:
             # print(event.type)
             if event.type == SDL_QUIT:
@@ -40,28 +35,37 @@ def main():
                 break
 
             if event.type == SDL_JOYAXISMOTION:
-                if event.jaxis.value >= 3200 or event.jaxis.value <= -3200:
-                    if event.jaxis.axis == 1:
-                        l_axis = event.jaxis.value >> 8
-                        l_axis = l_axis + 128
-                        data = bytearray()
+                # if event.jaxis.value >= 3200 or event.jaxis.value <= -3200:
+                if event.jaxis.axis == 1:
+                    l_axis = event.jaxis.value >> 8
+                    l_axis = l_axis + 128
+                    data = bytearray()
 
-                        data.append(l_axis)
-                        data.append(r_axis)
-                        
+                    data.append(l_axis)
+                    data.append(r_axis)
+                    
+                    try:
                         sock.sendto(data, (HOST, PORT))
-                        print("Gamepad Left Y: %d, %d" % (event.jaxis.axis, event.jaxis.value))
-                    if event.jaxis.axis == 3:
-                        r_axis = event.jaxis.value >> 8
-                        r_axis = r_axis + 128
-                        data = bytearray()
+                    except:
+                        pass
+                    print("Gamepad Left Y: %d, %d" % (event.jaxis.axis, event.jaxis.value))
+                if event.jaxis.axis == 3:
+                    r_axis = event.jaxis.value >> 8
+                    r_axis = r_axis + 128
+                    data = bytearray()
 
-                        data.append(l_axis)
-                        data.append(r_axis)
-                        
+                    data.append(l_axis)
+                    data.append(r_axis)
+                    
+                    try:
                         sock.sendto(data, (HOST, PORT))
-                        print("Gamepad Right Y: %d, %d" % (event.jaxis.axis, event.jaxis.value))
+                    except:
+                        pass
+                    print("Gamepad Right Y: %d, %d" % (event.jaxis.axis, event.jaxis.value))
                 break
+
+
+
 
     SDL_JoystickClose(gamepad)
     SDL_DestroyWindow(window)
